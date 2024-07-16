53f11915d2e4dc2cc8685d1cd77e53ceb9c16413
Ninja07-95 <anassbarriz7@gmail.com>
2024-07-16 09:45:59 +0200

    hopefully

--- src/main.rs
+++ src/main.rs
@@ -1,20 +1,34 @@
 mod utils;
 
-use utils::{create_pass, PassConfig};
+use utils::{create_pass, create_passphrase, PassConfig, save_to_file};
 use std::{env, io::Write};
+use termcolor::{Color, ColorChoice, ColorSpec, StandardStream, WriteColor};
+use simplelog::{Config, LevelFilter, SimpleLogger};
 
 fn main() {
+    SimpleLogger::init(LevelFilter::Info, Config::default()).unwrap();
+
     let config = env::args().collect::<Vec<String>>();
     let mut has_nums = true;
     let mut has_symbols = true;
+    let mut has_lowercase = true;
+    let mut has_uppercase = true;
     let mut length = 10;
+    let mut num_passwords = 1;
     let mut filename = None;
+    let mut use_passphrase = false;
+    let mut passphrase_words = 4;
     let help_msg = "passgen [OPTIONS]
 
     OPTIONS:
         -nn | --no-nums         No numbers
-        -ns | -no-symbols       No symbols
+        -ns | --no-symbols      No symbols
+        -nl | --no-lowercase    No lowercase letters
+        -nu | --no-uppercase    No uppercase letters
         -l  | --len <len>       Specify password length
+        -n  | --num <num>       Number of passwords to generate
+        -p  | --passphrase      Generate passphrases instead of passwords
+        -w  | --words <words>   Number of words in passphrase (default: 4)
         -h  | --help            Display this message
         -o  | --out <file>      Save to file
     ";
@@ -29,11 +43,68 @@ fn main() {
             "--no-symbols" | "-ns" => {
                 has_symbols = false;
             }
+            "--no-lowercase" | "-nl" => {
+                has_lowercase = false;
+            }
+            "--no-uppercase" | "-nu" => {
+                has_uppercase = false;
+            }
             "--out" | "-o" => {
                 filename = Some(config[index + 1].to_string());
             }
             "--len" | "-l" => {
-                length = config[index + 1].parse().unwrap();
+                length = match config.get(index + 1) {
+                    Some(len) => match len.parse::<usize>() {
+                        Ok(l) if l > 0 => l,
+                        _ => {
+                            eprintln!("Error: Invalid length '{}'\n", len);
+                            println!("{}", help_msg);
+                            std::process::exit(1);
+                        }
+                    },
+                    None => {
+                        eprintln!("Error: No length provided\n");
+                        println!("{}", help_msg);
+                        std::process::exit(1);
+                    }
+                };
+            }
+            "--num" | "-n" => {
+                num_passwords = match config.get(index + 1) {
+                    Some(num) => match num.parse::<usize>() {
+                        Ok(n) if n > 0 => n,
+                        _ => {
+                            eprintln!("Error: Invalid number of passwords '{}'\n", num);
+                            println!("{}", help_msg);
+                            std::process::exit(1);
+                        }
+                    },
+                    None => {
+                        eprintln!("Error: No number of passwords provided\n");
+                        println!("{}", help_msg);
+                        std::process::exit(1);
+                    }
+                };
+            }
+            "--passphrase" | "-p" => {
+                use_passphrase = true;
+            }
+            "--words" | "-w" => {
+                passphrase_words = match config.get(index + 1) {
+                    Some(words) => match words.parse::<usize>() {
+                        Ok(w) if w > 0 => w,
+                        _ => {
+                            eprintln!("Error: Invalid number of words '{}'\n", words);
+                            println!("{}", help_msg);
+                            std::process::exit(1);
+                        }
+                    },
+                    None => {
+                        eprintln!("Error: No number of words provided\n");
+                        println!("{}", help_msg);
+                        std::process::exit(1);
+                    }
+                };
             }
             "--help" | "-h" => {
                 println!("{}", help_msg);
@@ -53,13 +124,45 @@ fn main() {
         std::process::exit(1);
     }
 
-    let password = create_pass(PassConfig {
+    let pass_config = PassConfig {
         length,
         has_nums,
         has_symbols,
-        filename,
-    });
+        has_lowercase,
+        has_uppercase,
+        filename: filename.clone(),
+    };
 
-    writeln!(std::io::stdout(), "Generated Password:").unwrap();
-    writeln!(std::io::stdout(), "{}", password).unwrap();
+    let mut stdout = StandardStream::stdout(ColorChoice::Always);
+    let mut passwords = Vec::new();
+
+    if use_passphrase {
+        log::info!("Generating passphrases");
+        for _ in 0..num_passwords {
+            let passphrase = create_passphrase(passphrase_words);
+            passwords.push(passphrase);
+        }
+    } else {
+        log::info!("Generating passwords");
+        for _ in 0..num_passwords {
+            let password = create_pass(&pass_config);
+            passwords.push(password);
+        }
+    }
+
+    stdout.set_color(ColorSpec::new().set_fg(Some(Color::Yellow)).set_bold(true)).unwrap();
+    writeln!(stdout, "Generated {}: ", if use_passphrase { "Passphrases" } else { "Passwords" }).unwrap();
+
+    for password in &passwords {
+        stdout.set_color(ColorSpec::new().set_fg(Some(Color::Rgb(255, 165, 0))).set_bold(false)).unwrap();
+        writeln!(stdout, "{}", password).unwrap();
+    }
+
+    if let Some(filename) = filename {
+        if let Err(e) = save_to_file(&filename, &passwords) {
+            eprintln!("Error saving passwords to file: {}", e);
+        } else {
+            log::info!("Passwords saved to file '{}'", filename);
+        }
+    }
 }
--- src/utils.rs
+++ src/utils.rs
@@ -6,13 +6,21 @@ pub struct PassConfig {
     pub length: usize,
     pub has_nums: bool,
     pub has_symbols: bool,
+    pub has_lowercase: bool,
+    pub has_uppercase: bool,
     pub filename: Option<String>,
 }
 
-pub fn create_pass(config: PassConfig) -> String {
+pub fn create_pass(config: &PassConfig) -> String {
     let mut rng = rand::thread_rng();
-    let mut characters: Vec<char> = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ".chars().collect();
+    let mut characters = Vec::new();
 
+    if config.has_lowercase {
+        characters.extend("abcdefghijklmnopqrstuvwxyz".chars());
+    }
+    if config.has_uppercase {
+        characters.extend("ABCDEFGHIJKLMNOPQRSTUVWXYZ".chars());
+    }
     if config.has_nums {
         characters.extend("0123456789".chars());
     }
@@ -20,19 +28,34 @@ pub fn create_pass(config: PassConfig) -> String {
         characters.extend("!@#$%^&*()_+-=[]{}|;:'\",.<>?/`~".chars());
     }
 
-    let password: String = (0..config.length)
-        .map(|_| characters[rng.gen_range(0..characters.len())])
-        .collect();
-
-    if let Some(filename) = config.filename {
-        save_to_file(&filename, &password).expect("Failed to save the password to file");
+    if characters.is_empty() {
+        panic!("No character sets selected. Enable at least one character set.");
     }
 
-    password
+    (0..config.length)
+        .map(|_| characters[rng.gen_range(0..characters.len())])
+        .collect()
 }
 
-fn save_to_file(filename: &str, password: &str) -> io::Result<()> {
+pub fn create_passphrase(num_words: usize) -> String {
+    let words = vec![
+        "123456", "12345","123456789","password","iloveyou","princess","1234567","12345678","abc123","nicole","daniel","babygirl","monkey","lovely","jessica","654321","michael","ashley","qwerty","111111","iloveu","000000","michelle","tigger","sunshine","chocolate","apple", "banana", "cherry", "date", "elderberry", "fig", "grape", "honeydew",
+        "indigo", "jackfruit", "kiwi", "lemon", "mango", "nectarine", "orange", "papaya",
+        "quince", "raspberry", "strawberry", "tangerine", "ugli", "vanilla", "watermelon",
+        "xigua", "yellowfruit", "zucchini",
+    ];
+
+    let mut rng = rand::thread_rng();
+    (0..num_words)
+        .map(|_| words[rng.gen_range(0..words.len())].to_string())
+        .collect::<Vec<_>>()
+        .join(" ")
+}
+
+pub fn save_to_file(filename: &str, passwords: &[String]) -> io::Result<()> {
     let mut file = File::create(filename)?;
-    writeln!(file, "{}", password)?;
+    for password in passwords {
+        writeln!(file, "{}", password)?;
+    }
     Ok(())
 }
